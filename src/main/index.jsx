5ebd675baae7f22391d1e819f1babf02fe969490
Jonah Polack <jonah.polack@cleverbridge.com>
2018-03-24 22:10:46 +0100

    style

--- src/main/index.jsx
+++ src/main/index.jsx
@@ -4,38 +4,57 @@ import { withRouter } from 'react-router-dom';
 import View from './view';
 
 
-const onSubmit = async (values) => {
-  console.log(values);
-  const result = await fetch('http://localhost:8000/new', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({
-      capsule: {
-        title: values.title,
-        subtitle: values.subtitle,
-        from: values.from,
+class Main extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      loading: false,
+    };
+
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onClose = this.onClose.bind(this);
+  }
+
+  onClose() {
+    this.setState({
+      link: undefined,
+    });
+  }
+
+  async onSubmit(values) {
+    this.setState({
+      loading: true,
+    });
+    const capsule = await fetch('http://localhost:8000/new', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
       },
-      openingDate: new Date(),
-      password: 'abcabcabcabcabcd',
-    }),
-  }).then(res => res.text());
+      body: JSON.stringify({
+        capsule: {
+          title: values.title,
+          subtitle: values.subtitle,
+          from: values.from,
+        },
+        openingDate: new Date(),
+        password: values.password,
+      }),
+    }).then(res => res.json());
 
-  console.log('result', result);
-};
+    this.setState({
+      loading: false,
+      link: capsule.link,
+    });
+    console.log('capsule', capsule.link);
+  }
 
-const Main = () => {
-  return (
-    <View onSubmit={onSubmit} />
-  );
-};
-
-Main.propTypes = {
-};
-
-Main.defaultProps = {
-};
+  render() {
+    return (
+      <View onSubmit={this.onSubmit} onClose={this.onClose} loading={this.state.loading} link={this.state.link} />
+    );
+  }
+}
 
 
 export default withRouter(connect()(Main));
--- src/main/view.jsx
+++ src/main/view.jsx
@@ -4,45 +4,95 @@ import Button from 'material-ui/Button';    // BUTTON
 import Input, { InputLabel } from 'material-ui/Input';    // INPUT
 import { FormControl } from 'material-ui/Form';
 import { Field, reduxForm } from 'redux-form';
+import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import { CircularProgress } from 'material-ui/Progress';
 
 import '../assets/static/app.css';
 
-const CustomTextField = ({ input, label }) =>
+const CustomTextField = ({ input, label, contentType }) =>
   <div className="wrapper_inputFeld" >
     <FormControl className="inputFeld">
-      <InputLabel htmlFor="name-simple">{label}</InputLabel>
-      <Input id="name-simple" {...input} />
+      <InputLabel>{label}</InputLabel>
+      <Input {...input} type={contentType} />
     </FormControl>
   </div>;
 
 CustomTextField.propTypes = {
   label: PropTypes.string.isRequired,
+  contentType: PropTypes.string,
   input: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
 };
+CustomTextField.defaultProps = {
+  contentType: 'text',
+};
 
-export const App = ({ handleSubmit, onSubmit }) => {
+const renderLoading = () => (
+  <Dialog open aria-labelledby="simple-dialog-title">
+    <DialogTitle id="simple-dialog-title">Zeitkapsel wird erstellt</DialogTitle>
+    <div style={{ textAlign: 'center', padding: 20 }}>
+      <CircularProgress />
+    </div>
+  </Dialog>
+);
+
+const showLink = (link, onClose) => (
+  <Dialog open aria-labelledby="simple-dialog-title" onClose={onClose}>
+    <DialogTitle id="simple-dialog-title">Die Zeitkapsel steht bereit</DialogTitle>
+    <div style={{ textAlign: 'center', padding: 20, paddingTop: 0 }}>
+      <p>Sichere den Link, um auf deine Zeitkapsel zuzugreifen</p>
+      <Input
+        multiline
+        value={link}
+        onClick={event => event.target.select()}
+      />
+      <div
+        style={{
+          marginTop: 10,
+        }}
+      >
+        <Button onClick={onClose} color="primary" variant="raised">
+          Alles klar ich habe den Link gespeichert
+        </Button>
+      </div>
+    </div>
+  </Dialog>
+);
+
+export const App = ({ handleSubmit, onSubmit, loading, link, onClose }) => {
   return (
-    <form onSubmit={handleSubmit(onSubmit)}>
-      <div className="box-build">
-        <h1>Erstell deine eigene Zeitkapsel für dich und deine Freunde</h1>
-        <div className="contactForm">
-          <Field name="title" component={CustomTextField} label="Titel deiner Zeitkapsel" />
-          <Field name="subtitle" component={CustomTextField} label="Beschreibung (optional)" />
-          <Field name="from" component={CustomTextField} label="Von wem ist diese Zeitkapsel?" />
-          <Field name="openingDate" component={CustomTextField} label="Öffnungsdatum" />
-          <Button variant="raised" color="secondary" type="submit">
+    <div>
+      {loading && renderLoading()}
+      {link && showLink(link, onClose)}
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div className="box-build">
+          <h1>Erstelle eine Zeitkapsel für dich und deine Freunde</h1>
+          <div className="contactForm">
+            <Field name="title" component={CustomTextField} label="Titel deiner Zeitkapsel" />
+            <Field name="subtitle" component={CustomTextField} label="Beschreibung (optional)" />
+            <Field name="from" component={CustomTextField} label="Von wem ist diese Zeitkapsel?" />
+            <Field name="openingDate" component={CustomTextField} label="Öffnungsdatum" />
+            <Field name="password" component={CustomTextField} label="Passwort" contentType="password" />
+            <Button variant="raised" color="primary" type="submit">
               Jetzt Erstellen
           </Button>
+          </div>
         </div>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 
 
 App.propTypes = {
+  link: PropTypes.string,
+  loading: PropTypes.bool.isRequired,
   handleSubmit: PropTypes.func.isRequired,
+  onClose: PropTypes.func.isRequired,
   onSubmit: PropTypes.func.isRequired,
+};
+
+App.defaultProps = {
+  link: undefined,
 };
 
 export default reduxForm({
