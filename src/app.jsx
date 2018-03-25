1993d373c937f349c6245efeb8940ee40d3daf07
Jonah Polack <jonah.polack@cleverbridge.com>
2018-03-25 10:26:43 +0200

    menu

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,27 @@
 import React from 'react';
-import { Route, BrowserRouter as Router, Switch, Redirect } from 'react-router-dom';
+import { Route, BrowserRouter as Router, Switch, Redirect, Link } from 'react-router-dom';
 
 import MainComp from './create';
 import WriteComp from './write';
 import ShowComp from './show';
+import ProxyShow from './proxyShow';
 
 export const App = () => {
   return (
     <Router>
       <div>
+        <nav style={{ position: 'absolute', right: 50, top: 50 }}>
+          <Link to="/">
+            <label className="menuItem">Zeitkapsel erstellen</label>
+          </Link>
+          <Link to="/open">
+            <label className="menuItem">Zeitkapsel öffnen</label>
+          </Link>
+        </nav>
         <Switch>
           <Route exact path="/" component={MainComp} />
           <Route path="/capsule/:id" component={WriteComp} />
+          <Route path="/open" component={ProxyShow} />
           <Route path="/show/:id" component={ShowComp} />
           <Redirect to="/" />
         </Switch>
---
+++ src/proxyShow/index.jsx
@@ -0,0 +1,79 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { reduxForm, Field } from 'redux-form';
+import Dialog, { DialogTitle } from 'material-ui/Dialog';
+import Input, { InputLabel } from 'material-ui/Input';    // INPUT
+import Button from 'material-ui/Button';    // INPUT
+import { FormControl } from 'material-ui/Form';
+import { withRouter } from 'react-router-dom';
+
+import moment from 'moment';
+
+moment.locale('de');
+
+const CustomTextField = ({ input, label, contentType }) =>
+  <div className="wrapper_inputFeld" >
+    <FormControl className="inputFeld">
+      <InputLabel>{label}</InputLabel>
+      <Input {...input} type={contentType} />
+    </FormControl>
+  </div>;
+
+CustomTextField.propTypes = {
+  label: PropTypes.string.isRequired,
+  contentType: PropTypes.string,
+  input: PropTypes.object.isRequired, // eslint-disable-line react/forbid-prop-types
+};
+CustomTextField.defaultProps = {
+  contentType: 'text',
+};
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+
+    this.submit = this.submit.bind(this);
+  }
+
+  submit(values) {
+    this.props.history.push(`/show/${values.id}`);
+  }
+
+  render() {
+    const handleSubmit = this.props.handleSubmit;
+    return (
+      <div className="wrapper wrapperWrite">
+        <img
+          src="/logo.png" width="200" alt="logo" style={{
+            marginLeft: 50,
+          }}
+        />
+        <Dialog open aria-labelledby="simple-dialog-title">
+          <DialogTitle id="simple-dialog-title">Gib die Zeitkapsel ID ein</DialogTitle>
+          <div style={{ textAlign: 'center', padding: 20 }}>
+            <form onSubmit={handleSubmit(this.submit)}>
+              <Field name="id" component={CustomTextField} label="Zeitkapsel ID" />
+              <Button style={{ marginTop: 20 }} variant="raised" color="primary" type="submit">
+                Jetzt Erstellen
+              </Button>
+            </form>
+          </div>
+        </Dialog>
+      </div>
+    );
+  }
+}
+
+App.propTypes = {
+  handleSubmit: PropTypes.func.isRequired,
+  history: PropTypes.shape({
+    push: PropTypes.func.isRequired,
+  }).isRequired,
+};
+
+export default withRouter(reduxForm({
+  form: 'kapselId',
+})(App));
+
